a4f99c03cdfc1a5e92074c0e92ddabc13c5560ca
MichaelNest <nesterenkom385@gmail.com>
2020-08-04 21:43:40 +0300

    Create functions post_list and post_detail in view.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blog/list.html', context)
+
+def post_detail(request, year, month, day, post): # по этим параметрам будет найден пост в базе данных
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
+
+
